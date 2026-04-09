34d447e22e2ecfd421a08264524d6d07a7c54cae
mclaudiarocha <124476560+mclaudiarocha@users.noreply.github.com>
2026-04-09 00:14:49 -0300

    refactor: melhora construtor

--- projeto/hello-world/hello-world.cpp
+++ projeto/hello-world/hello-world.cpp
@@ -6,7 +6,9 @@ public:
     std::string nome;
     int idade;
 
-    Usuario(std::string n, int i) : nome(n), idade(i) {}
+    Usuario(std::string n, int i) : nome(n), idade(i) {
+    if (idade < 0) idade = 0;
+    }
 
     void apresentar() {
         std::cout << "Olá, meu nome é " << nome << " e tenho " << idade << " anos." << std::endl;
