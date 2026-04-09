b297f992e6e6eddbfd3a38d24c0c40eec82b14a6
mclaudiarocha <124476560+mclaudiarocha@users.noreply.github.com>
2026-04-08 23:50:44 -0300

    feat: adiciona classe Usuario

--- projeto/hello-world/hello-world.cpp
+++ projeto/hello-world/hello-world.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+class Usuario {
+public:
+    std::string nome;
+    int idade;
+
+    Usuario(std::string n, int i) : nome(n), idade(i) {}
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
